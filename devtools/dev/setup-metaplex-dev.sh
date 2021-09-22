d550831381b24031e2607676f5e8799e3d5cf9a4
Ashish <ashish@Ashishs-MacBook.local>
2021-09-22 11:43:58 +0530

    yes

--- devtools/dev/setup-metaplex-dev.sh
+++ devtools/dev/setup-metaplex-dev.sh
@@ -1,8 +1,8 @@
 echo "[INFO] Cleaning older cache folder for metaplex"
 rm -rf ./.cache
 echo "[INFO] Uploading all resources"
-metaplex upload ./nfts-sources --env devnet --keypair ~/.config/solana/candyfactory-devnet.json > ./logs/dev/upload-log.txt
+metaplex upload ./nfts-sources --env devnet --keypair /Users/ashish/.config/solana/devnet.json > ./logs/dev/upload-log.txt
 echo "[INFO] Creating candy machine"
-metaplex create_candy_machine --env devnet --keypair ~/.config/solana/candyfactory-devnet.json --price 1 > ./logs/dev/candy-machine-log.txt
+metaplex create_candy_machine --env devnet --keypair /Users/ashish/.config/solana/devnet.json --price 0.5 > ./logs/dev/candy-machine-log.txt
 echo "[INFO] Setting minting start date (goLiveDate)"
-metaplex update_candy_machine -d "25 Sep 2021 00:00:00" --env devnet --keypair ~/.config/solana/candyfactory-devnet.json > ./logs/dev/candy-machine-start-date.txt
+metaplex update_candy_machine -d "16 Sep 2021 00:00:00" --env devnet --keypair /Users/ashish/.config/solana/devnet.json > ./logs/dev/candy-machine-start-date.txt
--- devtools/dev/setup-solana-dev.sh
+++ devtools/dev/setup-solana-dev.sh
@@ -1,8 +1,7 @@
 echo "[INFO] Setting Solana to use dev network"
 solana config set --url https://api.devnet.solana.com
 echo "[INFO] Generating new wallet"
-solana-keygen new --outfile ~/.config/solana/candyfactory-devnet.json --force > ./logs/dev/new-wallet-log.txt
+
 echo "[INFO] Setting config to be the new wallet"
-solana config set -k ~/.config/solana/candyfactory-devnet.json
+solana config set -k /Users/ashish/.config/solana/devnet.json
 echo "[INFO] Sending airdrop"
-solana airdrop 10
